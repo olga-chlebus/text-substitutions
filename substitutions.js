ce5f26fedf2120308b134e3fef7b450d530b7dbf
Olga Chlebus <olga.chlebus@gmail.com>
2016-01-06 17:45:14 +0100

    Some improvements
    Fewer RegExp objects created.
    Only non-empty text nodes analysed.
    No global variables.

--- substitutions.js
+++ substitutions.js
@@ -1,28 +1,111 @@
-var walker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
-var currentTextNode;
-var textReplace = function(node, textToReplace, textToReplaceWith){
-    var regExp = new RegExp(textToReplace, 'ig');
-    node.data = node.data.replace(regExp, textToReplaceWith);
-}
-while (currentTextNode = walker.nextNode()){
-    textReplace(currentTextNode, 'debate', 'dance-off');
-    textReplace(currentTextNode, 'self-driving', 'uncontrollably swerving');
-    textReplace(currentTextNode, 'poll', 'psychic reading');
-    textReplace(currentTextNode, 'candidate', 'airbender');
-    textReplace(currentTextNode, 'drone', 'dog');
-    textReplace(currentTextNode, 'vows to', 'probably won\'t');
-    textReplace(currentTextNode, 'at large', 'very large');
-    textReplace(currentTextNode, 'successfully', 'suddenly');
-    textReplace(currentTextNode, 'expands', 'physically expands');
-    textReplace(currentTextNode, 'first degree', 'friggin\' awful');
-    textReplace(currentTextNode, 'second degree', 'friggin\' awful');
-    textReplace(currentTextNode, 'third degree', 'friggin\' awful');
-    textReplace(currentTextNode, 'an unknown number', 'like hundreds');
-    textReplace(currentTextNode, 'front runner', 'blade runner');
-    textReplace(currentTextNode, 'global', 'spherical');
-    textReplace(currentTextNode, 'years', 'minutes');
-    textReplace(currentTextNode, 'minutes', 'years');
-    textReplace(currentTextNode, 'no indication', 'lots of signs');
-    textReplace(currentTextNode, 'urged restraint by', 'drunkenly egged on');
-    textReplace(currentTextNode, 'horsepower', 'tons of horsemeat');
-}
+(function(){
+	var textSubstitutions = [
+		{
+			textToReplace: 'debate',
+			textToReplaceWith: 'dance-off'
+		},
+		{
+			textToReplace: 'self driving',
+			textToReplaceWith: 'uncontrollably swerving'
+		},
+		{
+			textToReplace: 'poll',
+			textToReplaceWith: 'psychic reading'
+		},
+		{
+			textToReplace: 'candidate',
+			textToReplaceWith: 'airbender'
+		},
+		{
+			textToReplace: 'drone',
+			textToReplaceWith: 'dog'
+		},
+		{
+			textToReplace: 'vows to',
+			textToReplaceWith: 'probably won\'t'
+		},
+		{
+			textToReplace: 'at large',
+			textToReplaceWith: 'very large'
+		},
+		{
+			textToReplace: 'successfully',
+			textToReplaceWith: 'suddenly'
+		},
+		{
+			textToReplace: 'expands',
+			textToReplaceWith: 'physically expands'
+		},
+		{
+			textToReplace: 'first degree',
+			textToReplaceWith: 'friggin\' awful'
+		},
+		{
+			textToReplace: 'second degree',
+			textToReplaceWith: 'friggin\' awful'
+		},
+		{
+			textToReplace: 'third degree',
+			textToReplaceWith: 'friggin\' awful'
+		},
+		{
+			textToReplace: 'an unknown number',
+			textToReplaceWith: 'like hundreds'
+		},
+		{
+			textToReplace: 'front runner',
+			textToReplaceWith: 'blade runner'
+		},
+		{
+			textToReplace: 'global',
+			textToReplaceWith: 'spherical'
+		},
+		{
+			textToReplace: 'years',
+			textToReplaceWith: 'minutes'
+		},
+		{
+			textToReplace: 'minutes',
+			textToReplaceWith: 'years'
+		},
+		{
+			textToReplace: 'no indication',
+			textToReplaceWith: 'lots of signs'
+		},
+		{
+			textToReplace: 'urged restraint by',
+			textToReplaceWith: 'drunkenly egged on'
+		},
+		{
+			textToReplace: 'horsepower',
+			textToReplaceWith: 'tons of horsemeat'
+		}
+	];
+
+	var regexpSubstitutions = [];
+	for(i in textSubstitutions) {
+		var rSubst = {
+			regexpToReplace: new RegExp(textSubstitutions[i].textToReplace, 'ig'),
+			textToReplaceWith: textSubstitutions[i].textToReplaceWith
+		};
+		regexpSubstitutions.push(rSubst);
+	};
+
+	var replace = function(node, regexpSubstitution) {
+	    node.data = node.data.replace(regexpSubstitution.regexpToReplace, regexpSubstitution.textToReplaceWith);
+	}
+
+	var walker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
+	var currentTextNode;
+
+
+	while (currentTextNode = walker.nextNode()) {
+		if (currentTextNode.data.trim()) {
+			for(i in regexpSubstitutions) {
+				replace(currentTextNode,regexpSubstitutions[i]);
+			}
+
+		}
+	}
+
+})();
