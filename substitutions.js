13979c0c396f473970508e55764dfd125bbf340f
olga-chlebus <olga-chlebus@users.noreply.github.com>
2017-06-20 22:11:17 +0100

    Refactor, cycle bug fixed

--- substitutions.js
+++ substitutions.js
@@ -1,111 +1,51 @@
 (function(){
-	var textSubstitutions = [
-		{
-			textToReplace: 'debate',
-			textToReplaceWith: 'dance-off'
-		},
-		{
-			textToReplace: 'self driving',
-			textToReplaceWith: 'uncontrollably swerving'
-		},
-		{
-			textToReplace: 'poll',
-			textToReplaceWith: 'psychic reading'
-		},
-		{
-			textToReplace: 'candidate',
-			textToReplaceWith: 'airbender'
-		},
-		{
-			textToReplace: 'drone',
-			textToReplaceWith: 'dog'
-		},
-		{
-			textToReplace: 'vows to',
-			textToReplaceWith: 'probably won\'t'
-		},
-		{
-			textToReplace: 'at large',
-			textToReplaceWith: 'very large'
-		},
-		{
-			textToReplace: 'successfully',
-			textToReplaceWith: 'suddenly'
-		},
-		{
-			textToReplace: 'expands',
-			textToReplaceWith: 'physically expands'
-		},
-		{
-			textToReplace: 'first degree',
-			textToReplaceWith: 'friggin\' awful'
-		},
-		{
-			textToReplace: 'second degree',
-			textToReplaceWith: 'friggin\' awful'
-		},
-		{
-			textToReplace: 'third degree',
-			textToReplaceWith: 'friggin\' awful'
-		},
-		{
-			textToReplace: 'an unknown number',
-			textToReplaceWith: 'like hundreds'
-		},
-		{
-			textToReplace: 'front runner',
-			textToReplaceWith: 'blade runner'
-		},
-		{
-			textToReplace: 'global',
-			textToReplaceWith: 'spherical'
-		},
-		{
-			textToReplace: 'years',
-			textToReplaceWith: 'minutes'
-		},
-		{
-			textToReplace: 'minutes',
-			textToReplaceWith: 'years'
-		},
-		{
-			textToReplace: 'no indication',
-			textToReplaceWith: 'lots of signs'
-		},
-		{
-			textToReplace: 'urged restraint by',
-			textToReplaceWith: 'drunkenly egged on'
-		},
-		{
-			textToReplace: 'horsepower',
-			textToReplaceWith: 'tons of horsemeat'
-		}
-	];
+    var textSubstitutions = {
+        'debate': 'dance-off',
+        'self driving': 'uncontrollably swerving',
+        'poll': 'psychic reading',
+        'candidate': 'airbender',
+        'drone': 'dog',
+        'vows to': 'probably won\'t',
+        'at large': 'very large',
+        'successfully': 'suddenly',
+        'expands': 'physically expands',
+        'first degree': 'friggin\' awful',
+        'second degree': 'friggin\' awful',
+        'third degree': 'friggin\' awful',
+        'an unknown number': 'like hundreds',
+        'front runner': 'blade runner',
+        'global': 'spherical',
+        'years': 'temp1',
+        'minutes': 'years',
+        'no indication': 'lots of signs',
+        'urged restraint by': 'drunkenly egged on',
+        'horsepower': 'tons of horsemeat',
+        'temp1': 'minutes'
+    };
 
-	var regexpSubstitutions = [];
-	for(i in textSubstitutions) {
-		var rSubst = {
-			regexpToReplace: new RegExp(textSubstitutions[i].textToReplace, 'ig'),
-			textToReplaceWith: textSubstitutions[i].textToReplaceWith
-		};
-		regexpSubstitutions.push(rSubst);
-	};
+    var regexpSubstitutions = [];
+    for(key in textSubstitutions) {
+        var rSubst = {
+            regexpToReplace: new RegExp(key, 'ig'),
+            textToReplaceWith: textSubstitutions[key]
+        };
+        regexpSubstitutions.push(rSubst);
+    };
 
-	var replace = function(node, regexpSubstitution) {
-	    node.data = node.data.replace(regexpSubstitution.regexpToReplace, regexpSubstitution.textToReplaceWith);
-	}
+    var replace = function(node, regexpSubstitution) {
+        node.data = node.data.replace(regexpSubstitution.regexpToReplace, regexpSubstitution.textToReplaceWith);
+    }
 
-	var walker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
-	var currentTextNode;
+    var walker = document.createTreeWalker(document, NodeFilter.SHOW_TEXT, null, false);
+    var currentTextNode;
 
 
-	while (currentTextNode = walker.nextNode()) {
-		if (currentTextNode.data.trim()) {
-			for(i in regexpSubstitutions) {
-				replace(currentTextNode,regexpSubstitutions[i]);
-			}
-
-		}
-	}
+    while (currentTextNode = walker.nextNode()) {
+        if (currentTextNode.data.trim()) {
+            for(i in regexpSubstitutions) {
+                replace(currentTextNode,regexpSubstitutions[i]);
+            }
+        }
+    }
 
 })();
